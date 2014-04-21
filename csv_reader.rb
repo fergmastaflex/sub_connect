02564b4e680e40d4e20d04c39659a9206f69cda3
ferg <joey@fergmedia.com>
2014-04-21 15:50:58 -0600

    Adds count of wellformed users by company.

--- csv_reader.rb
+++ csv_reader.rb
@@ -4,12 +4,6 @@ class CsvReader
 
   attr_accessor :users, :businesses
 
-  # def read_in_csv(filename)
-  #   CSV.foreach(filename, headers: true) do |row|
-  #     puts row["Name"]
-  #   end
-  # end
-
   def parse_business
     @businesses = []
     CSV.foreach("businesses.csv", headers: true) do |row|
@@ -54,7 +48,7 @@ class CsvReader
     parse_users.count + parse_business.count
   end
 
-  def count_wellformed
+  def count_total_wellformed
     convert_csv_to_hash
     count = 0
     @hash_table.each do |b|
@@ -65,11 +59,69 @@ class CsvReader
     count
   end
 
+  def count_wellformed_business
+    parse_business
+    @hash_table = []
+    count = 0
+    @businesses.each do |b|
+      @hash_table << b.to_hash
+    end
+
+    @hash_table.each do |b|
+      unless b.has_value?(nil)
+        count += 1
+      end
+    end
+    count
+  end
+
+  def count_wellformed_users
+    parse_users
+    @hash_table = []
+    company_list = []
+    count = Hash.new(0)
+
+    @users.each do |u|
+      @hash_table << u.to_hash
+    end
+
+    @hash_table.each do |u|
+      if u["BusinessName"] != nil
+        company_list << u["BusinessName"]
+      end
+    end
+    
+    company_list.each {|name| count[name] += 1}
+    count.each {|key, value| print "#{key}: #{value} \n"}
+    return nil
+  end
+
+  def parse_wellformed_business
+    parse_business
+    @hash_table = []
+    @businesses.each do |b|
+      @hash_table << b.to_hash
+    end
+    @hash_table.each do |b|
+      unless b.has_value?(nil)
+        b.each {|key, value| print "#{key}: #{value} "}
+        puts "\n"
+      end
+    end
+    return nil
+  end
+
 end
 
 reader = CsvReader.new
 puts "Total records =  #{reader.get_total}"
 puts "Total malformed records = #{reader.count_malformed}"
-puts "Total well formed records = #{reader.count_wellformed}"
+puts "Total well formed records = #{reader.count_total_wellformed}"
+puts "Total well formed business records = #{reader.count_wellformed_business}"
+puts "Total wellformed user records by business:"
+puts reader.count_wellformed_users
+puts "Welformed businesses:"
+puts reader.parse_wellformed_business
+
 
 
